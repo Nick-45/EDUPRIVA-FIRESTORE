91da697f30cbc6e50dc097ad5dcdc4bb3a546198
NICKSON JAMES <obarenickson2@gmail.com>
2026-07-10 22:19:49 +0300

    Update AuthContext.js

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import { auth, db } from '../firebase/config';
+import { auth, db } from '../services/firebase';
 import {
 signInWithEmailAndPassword,
 signOut,
